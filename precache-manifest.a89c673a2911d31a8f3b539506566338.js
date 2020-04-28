28344fda38939fae3ac0854fb1ed147da6b2784d
sarangmane607 <62138683+sarangmane607@users.noreply.github.com>
2020-04-28 23:00:21 +0530

    Updates

---
+++ precache-manifest.a89c673a2911d31a8f3b539506566338.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8122a2fb3e6870f3decb1ee455d1626b",
+    "url": "/projet-sprint-react/index.html"
+  },
+  {
+    "revision": "7c97d41324f85f6360db",
+    "url": "/projet-sprint-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e8ff853b6481b511f331",
+    "url": "/projet-sprint-react/static/js/2.be79cef5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projet-sprint-react/static/js/2.be79cef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c97d41324f85f6360db",
+    "url": "/projet-sprint-react/static/js/main.dcb93385.chunk.js"
+  },
+  {
+    "revision": "7ef3a2a8a17dac445a37",
+    "url": "/projet-sprint-react/static/js/runtime-main.1c3ed0ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/projet-sprint-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
